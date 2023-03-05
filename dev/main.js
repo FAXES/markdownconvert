26019cde0e98c6b774def1d106a24afad15e427b
FAXES <31368070+FAXES@users.noreply.github.com>
2023-03-05 19:10:27 +1100

    Created for collapse HTTP responses

---
+++ dev/main.js
@@ -0,0 +1,12 @@
+
+window.onload = (event) => {
+    let httpResponses = document.querySelectorAll('[id^="httpReqRes-"]');
+    console.log(httpResponses)
+    httpResponses.forEach(e => {
+      e.onclick = function(){
+          let pre = document.getElementById(e.id.replace('httpReqRes-', ''));
+          if(pre.style.display == '') return pre.style.display = 'block';
+          pre.style.display == 'none' ? pre.style.display = 'block' : pre.style.display = 'none';
+      };
+    });
+};
