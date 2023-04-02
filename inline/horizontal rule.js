09cf21d05511a0d97aafecd1335a55c87409da6b
FAXES <31368070+FAXES@users.noreply.github.com>
2023-04-02 16:21:00 +1000

    Update horizontal rule.js

--- inline/horizontal rule.js
+++ inline/horizontal rule.js
@@ -3,6 +3,6 @@ const main = require('../index.js');
 main.registerInline(function(string) {
     let pre = '\n---\n';
     if(string.indexOf(pre) == -1) return false;
-    string = string.replace(pre, `<hr${main.getStyle('hr') ? ` class="${main.getStyle('hr')}"`: ''} />`);
+    string = string.replace(pre, `\n<hr${main.getStyle('hr') ? ` class="${main.getStyle('hr')}"`: ''} />\n`);
     return string;
 });
