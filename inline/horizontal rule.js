1174dd6023b2d67d868d42249f10879c64781740
FAXES <31368070+FAXES@users.noreply.github.com>
2024-08-30 15:24:02 +1000

    Update horizontal rule.js

--- inline/horizontal rule.js
+++ inline/horizontal rule.js
@@ -4,7 +4,7 @@ function convert(string, plain) {
     let pre = '\n---\n';
     if(string.indexOf(pre) == -1) return -1;
     if(plain) return string.replace(pre, '');
-    string = string.replace(pre, `\n<hr${main.getStyle('hr', true)} />\n`);
+    string = string.replace(pre, `<hr${main.getStyle('hr', true)} />`);
     return string;
 };
 
